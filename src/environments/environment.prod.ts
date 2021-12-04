c0a74cada84aab51d686b7002c6cf2e7b94ebeec
imsuck <49095435+imsuck@users.noreply.github.com>
2021-12-04 19:44:05 +0700

    Fix the whole thing (I hope it'll work now)

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    projectId: 'decoded-carport-332213',
+    appId: '1:397970110157:web:eb1173ff7ead4a56381b84',
+    databaseURL: 'https://decoded-carport-332213-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'decoded-carport-332213.appspot.com',
+    locationId: 'europe-west6',
+    apiKey: 'AIzaSyAoQLFvlVqrEDZ6_b3fKFZ9t6M8LOkhhNw',
+    authDomain: 'decoded-carport-332213.firebaseapp.com',
+    messagingSenderId: '397970110157',
+    measurementId: 'G-WPSZMRD6YL',
+  },
+  production: true
+};
